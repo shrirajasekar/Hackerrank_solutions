969aa4972cdb4a4d08c43dc3b323d5780c977289
shrirajasekar <shri.rajsekar@gmail.com>
2021-02-23 02:46:24 +0530

    Prev and Next Permutation

--- Problem Solving/NextLexicographyOrder/PreviousLexicographyOrder.java
+++ Problem Solving/NextLexicographyOrder/PreviousLexicographyOrder.java
@@ -6,30 +6,30 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class NextLexicographyOrder {
+public class PreviousLexicographyOrder {
 
-    static String nextPermutation(String w) {
+    static String prevPermutation(String w) {
         String result = "";
         StringBuilder s = new StringBuilder(w);
         int pivot;
-        // Find the non-increasing suffix from right to left
+        // Find the non-decreasing suffix from right to left
         int i = w.length() - 1; // Because we are going from right to left
-        while (i > 0 && Character.compare(s.charAt(i), s.charAt(i - 1)) <= 0)
+        while (i > 0 && Character.compare(s.charAt(i), s.charAt(i - 1)) > 0)
         // charAt i (Right) should be less than the one on the left (i-1)
         {
             i--; // charAt i-1 (left)
         }
-        if (i == 0) { // Already in last/largest permutation
+        if (i == 0) { // Already in first/least permutation
             result = "no answer";
             return result;
         }
         // Find the pivot point - One position to the left of 'Non-Increasing Suffix'
         pivot = i - 1; // i is the start of the suffix. So i-1 is the pivot
 
-        // Swap Pivot with the first greater than element from the right in suffix
+        // Swap Pivot with the first lesser than the element from the right in suffix
         int j = s.length() - 1;
         while (j > pivot) { // Traverse only through suffix
-            if (Character.compare(s.charAt(j), s.charAt(pivot)) > 0) {
+            if (Character.compare(s.charAt(j), s.charAt(pivot)) < 0) {
                 char pivotChar = s.charAt(pivot);
                 s.setCharAt(pivot, s.charAt(j));
                 s.setCharAt(j, pivotChar);
@@ -48,9 +48,9 @@ public class NextLexicographyOrder {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        String w = scanner.nextLine();
-        String result = nextPermutation(w);
-        System.out.println(result);
+            String w = scanner.nextLine();
+            String result = prevPermutation(w);
+            System.out.println(result);
         scanner.close();
     }
 }
