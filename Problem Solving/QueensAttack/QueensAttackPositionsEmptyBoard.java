1d21b8ae8096354a37a0756ebb6bb0308b32f7c7
shrirajasekar <shri.rajsekar@gmail.com>
2021-02-25 02:29:30 +0530

    Refactor

--- Problem Solving/QueensAttack/QueensAttackPositionsEmptyBoard.java
+++ Problem Solving/QueensAttack/QueensAttackPositionsEmptyBoard.java
@@ -9,25 +9,25 @@ import java.util.regex.*;
 public class QueensAttackPositionsEmptyBoard {
 
     // Find Queens Attack Position without considering any Obstacles / On an empty Board
-    static int queensAttack(int n, int r, int c) {
-        if (r > n || c > n) {
+    static int queensAttack(int n, int r_q, int c_q) {
+        if (r_q > n || c_q > n) {
             System.out.println("Invalid input");
             return 0;
         }
         // Find folded coordinates
-        int folded_r, folded_c;
+        int folded_r_q, folded_c_q;
         int totalAttackPositions;
-        folded_r = (n - r + 1);
-        folded_c = (n - c + 1);
+        folded_r_q = (n - r_q + 1);
+        folded_c_q = (n - c_q + 1);
         // Find the totalimum possible number of Attack Positions of the Queen
         // from Folded coordinates
-        if (folded_c < folded_r) {
+        if (folded_c_q < folded_r_q) {
             // Folded co-ord is to the left of the 'L' shaped region of same attacking power
-            totalAttackPositions = (n - 1) * 3 + (folded_r - 2) * 2; // Arithmetic progression
+            totalAttackPositions = (n - 1) * 3 + (folded_r_q - 2) * 2; // Arithmetic progression
         } else {
             // Folded co-ord is to the right of the 'L' shaped region of same attacking
             // power
-            totalAttackPositions = (n - 1) * 3 + (folded_r - 1) * 2; // Arithmetic progression
+            totalAttackPositions = (n - 1) * 3 + (folded_r_q - 1) * 2; // Arithmetic progression
         }
         return totalAttackPositions;
     }
@@ -40,13 +40,13 @@ public class QueensAttackPositionsEmptyBoard {
 
         int n = Integer.parseInt(nk[0]);
 
-        String[] rC = scanner.nextLine().split(" ");
+        String[] r_qC_q = scanner.nextLine().split(" ");
 
-        int r = Integer.parseInt(rC[0]);
+        int r_q = Integer.parseInt(r_qC_q[0]);
 
-        int c = Integer.parseInt(rC[1]);
+        int c_q = Integer.parseInt(r_qC_q[1]);
 
-        int result = queensAttack(n, r, c);
+        int result = queensAttack(n, r_q, c_q);
 
         System.out.println(String.valueOf(result));
         scanner.close();
